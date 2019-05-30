3f28b7cb454b6be2e13fce5b50a832082cf814dc
shivaprasad <as.prasad50@gmail.com>
2019-05-30 09:11:18 +0530

    JS Basics

---
+++ JS/1.js
@@ -0,0 +1,53 @@
+// 
+/**/
+
+// alert()
+// console.log();
+
+// document.write('demo....');
+
+// console.log(10 + 10);
+
+// console.log('shiva');
+// console.log('shivaprasad');
+
+
+// variables
+
+// Strings, number, boolean, null, undefined.
+// funcitons, arrays, objects. 
+
+var a = "10";
+// console.log( 10 + 10 + a / 10);
+var istrue = true;
+console.log(NValue);
+
+var NValue, demo, demo1, _demo2;
+NValue = "Shiva";
+
+
+
+demo();
+function demo(){
+    alert();
+}
+
+var demo1 = function(){
+    console.log('Hello...')
+}
+demo1();
+
+// Encapsulation...
+(function(){
+  console.log('Invoked...')  
+})();
+
+// Arrays;
+
+var user = ['DIGITAL LYNC','Shiva',30,20000, [11,22], {name:"Prasad", favColor:['red', 'green']}];
+console.log(user);
+
+
+var obj = {'FirstName':"Shiva", age:30, salary:20000};
+
+// console.log(obj);
